7dbc613c7dddebc40dc47e7a5bb0e7da5fcdea1a
Zhao Kai <snefrukai@gmail.com>
2021-03-31 02:47:17 +0800

    can't input multiple lines

---
+++ get_toc_short.py
@@ -0,0 +1,12 @@
+toc = """
+intro: asd
+Chapter 7: Getting Modular: Modules, methods, classes, and objects](zotero://open-pdf/0_MJMVS3VW/331)
+
+
+"""
+
+start = "chapter", "Chapter", "Introduction", "introduction", "intro:", "Intro:", "preface", "Preface", "Forward", "forward"
+
+for line in toc.splitlines():
+    if (line.startswith(start)):
+        print(line.capitalize())
